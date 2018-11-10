4908d467fc9446cf7a42f9bf9872ca4f9c5c5683
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-11-10 00:29:49 +0000

    feat: add prescription routes

---
+++ src/containers/Prescription/Create.vue
@@ -0,0 +1,49 @@
+C<template>
+	<b-container class="Unauthorized">
+
+		<span class="Unauthorized__title">Please <b-link @click="onLoginClick">login</b-link> or <b-link @click="onSignupClick">sign up!</b-link></span>
+
+	</b-container>
+
+</template>
+
+<script>
+export default {
+	methods: {
+		onLoginClick() {
+			this.$router.push({ query: { login: null } });
+		},
+		onSignupClick() {
+			this.$router.push({ query: { signUp: null } });
+		}
+	}
+}
+</script>
+
+
+<style lang="scss">
+
+@import '../../styles/index.scss';
+
+.Unauthorized {
+	@extend .inner;
+	height: 100%;
+	justify-content: center;
+	align-self: center;
+	margin-top: $headerHeight;
+
+	@media all and (min-width: $tablet) {
+		margin-top: 0
+	}
+
+	&__title {
+		font-size: 2rem;
+		width: auto;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+}
+
+</style>
+
---
+++ src/containers/Prescription/index.js
@@ -0,0 +1,12 @@
+
+import Create from './Create';
+
+const routes = [
+	{
+		name: 'prescription.create',
+		path: '/prescriptions/create',
+		component: Create,
+	}
+];
+
+export default routes;
