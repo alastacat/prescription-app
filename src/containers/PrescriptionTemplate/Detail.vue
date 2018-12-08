9843202f20a3782d93965fb8c56a1be44b5efefe
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2018-12-08 15:51:36 +0000

    Feat: information modules and loading states (#30)
    * feat: rename Survey to ModuleSurvey
    
    * fix: create route is no longer recognised as an id
    
    * feat: can now create a survey with information modules
    
    * fix: still displays questions when creating a new survey module
    
    * feat: list now displays counts of both surveys and information modules
    
    * fix: typo
    
    * feat: show information modules on the detail screen
    
    * feat: prescription-template detail screen design improvements
    
    * chore: rename ModuleCreate.modal to ModuleCreateModal and remove unused
    
    * feat: improve module detail styling and fix bugs
    
    * feat: add photo support
    
    * feat: add loading states globally

--- src/containers/PrescriptionTemplate/Detail.vue
+++ src/containers/PrescriptionTemplate/Detail.vue
@@ -8,25 +8,25 @@
 		<template v-else>
 
 			<h2 class="App__title" v-text="`Prescription Template: ${prescriptionTemplate.name}`"/>
+			<h5 class="TemplateDetail__author" v-text="prescriptionTemplate.author"/>
 
-			<h5 class="flex-start" v-text="prescriptionTemplate.author"/>
+			<section class="TemplateDetail__content">
+				<span class="App__label App__label--inline mt-3 w-25">Description:</span>
+				<span class="TemplateDetail__description" v-text="prescriptionTemplate.description"/>
+			</section>
 
-			<b-card class="App__card">
-				<b-list-group flush>
-					<b-list-group-item>
-						<span class="TemplateDetail__label">Description:</span>
-						<span v-text="prescriptionTemplate.description"/>
-					</b-list-group-item>
-					<b-list-group-item>
-						<span class="TemplateDetail__label">Surveys:</span>
-						<survey-detail
-							class="mb-1"
-							v-for="(survey, i) in prescriptionTemplate.surveys"
-							:key="i"
-							:survey="survey"/>
-					</b-list-group-item>
-				</b-list-group>
-			</b-card>
+			<section class="TemplateDetail__content">
+				<span class="App__label App__label--inline mt-3 w-25">Modules:</span>
+
+				<div class="TemplateDetail__modules">
+					<module-detail
+						class="TemplateDetail__module"
+						v-for="(module, i) in prescriptionTemplate.modules"
+						:key="i"
+						:module="module"/>
+				</div>
+
+			</section>
 
 		</template>
 
@@ -35,11 +35,11 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import SurveyDetail from './components/SurveyDetail';
+import ModuleDetail from './components/ModuleDetail';
 
 export default {
 	components: {
-		SurveyDetail
+		ModuleDetail
 	},
 	data() {
 		return {
@@ -77,12 +77,45 @@ export default {
 	@extend .flexbox;
 	padding: 1rem;
 
-	&__label {
-		font-weight: bold;
-		text-transform: uppercase;
-		display: block;
-		margin-bottom: 0.5rem;
+	&__author {
+		align-self: flex-start;
+		margin-bottom: 2rem;
 	}
+
+	&__content {
+		width: 100%;
+		@extend .flexbox;
+		flex-direction: row;
+		margin-bottom: 1rem;
+	}
+
+	&__description {
+		width: 100%;
+		min-height: 100px;
+		background: $white-true;
+		padding: 1rem;
+		text-align: left;
+	}
+
+	&__modules {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+	}
+
+	&__module {
+		width: 30%;
+		margin: 0 1.5%;
+		display: inline-block;
+
+		&:first-of-type {
+			margin-left: 0;
+		}
+		&:last-of-type {
+			margin-right: 0;
+		}
+	}
+
 }
 
 </style>
